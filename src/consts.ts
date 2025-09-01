b28768c8337892d3bb0f62eb040009d00243a7d0
eldho3 <156602595+eldho3@users.noreply.github.com>
2025-09-01 16:00:11 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// src/data/siteData.js
 
-export const SITE_TITLE = "Astro Blog";
+export const SITE_TITLE = "ELDHO SKARIA";
 export const SITE_DESCRIPTION = "Welcome to my website!";
